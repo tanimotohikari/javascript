874d52c833ce3b93d7dc62e232f77554f0544cb9
tanimotohikari <tanimoto.hikari@gmail.com>
2018-10-25 03:01:26 +0900

    update:master 左右のオセロ色の変更

--- index.js
+++ index.js
@@ -1,75 +1,120 @@
 window.onload = function(){
 
-  var $tableElements = document.getElementsByTagName('td');
+  let $tableElements = document.getElementsByTagName('td');
   //順番を制御するための変数
-  var order = true; //trueは黒（先行）
-  var othelloWhte = '◯';
-  var othelloBlack = '●';
-  var othelloColor = othelloBlack;
+  let order = true; //trueは黒（先行）
+  let othelloWhte = '◯';
+  let othelloBlack = '●';
+  let othelloColor = othelloBlack;
 
   //tableの全てにclickイベントを付与する
-  // for (var $i=0; $i < $tableElements.length; $i++) {
+  // for (let $i=0; $i < $tableElements.length; $i++) {
   //   othelloPositions.push($i);
   //   $tableElements[$i].addEventListener('click', function(){
-  //     var tableElements = [].slice.call($tableElements);
-  //     var index = tableElements.indexOf(this);
+  //     let tableElements = [].slice.call($tableElements);
+  //     let index = tableElements.indexOf(this);
   //     changeOthello(index);
   //     changeOrder();
   //   });
   // }
 
-  for (var i=0; i < $tableElements.length; i++) {
+  for (let i=0; i < $tableElements.length; i++) {
     $tableElements[i].addEventListener('click', function(){
       //配列に変換する
-      var tableElements = [].slice.call($tableElements);
+      let tableElements = [].slice.call($tableElements);
       //クリックした位置の取得
-      var index = tableElements.indexOf(this);
+      let index = tableElements.indexOf(this);
       putOthello(index);
+      changeOthello(index);
       changeOrder();
     });
   }
 
   //オセロを置く
-  // function putOthello(targetIndex, index) {
-  //   $tableElements[targetIndex].innerHTML = othelloColor;
-  //   $tableElements[index].innerHTML = othelloColor;
-  // }
+  function putOthello(targetIndex, index) {
+    $tableElements[targetIndex].innerHTML = othelloColor;
+    $tableElements[index].innerHTML = othelloColor;
+  }
 
   function putOthello(index) {
     $tableElements[index].innerHTML = othelloColor;
   }
 
   //オセロの色を変える
-  // function changeOthello(index) {
-  //   //両隣とその隣のオセロの色（値）を取得
-  //   var prevLeftOthello = $tableElements[index - 2].innerHTML;
-  //   var prevOthello = $tableElements[index - 1].innerHTML;
-  //   var nextRightOthello = $tableElements[index + 2].innerHTML;
-  //   var nextOthello = $tableElements[index + 1].innerHTML;
-  //
-  //   //左隣の次のマスの色が置いたオセロと同じ色の場合隣のオセロの色を変える
-  //   if (prevLeftOthello.match(othelloBlack) && prevOthello.match(othelloWhte)) {
-  //     var targetIndex = index - 1;
-  //     putOthello(targetIndex, index);
-  //   }
-  //
-  //   //右隣の次のマスの色が置いたオセロと同じ色の場合隣のオセロの色を変える
-  //   if (nextRightOthello.match(othelloBlack) && nextOthello.match(othelloWhte)) {
-  //     var targetIndex = index + 1;
-  //     putOthello(targetIndex, index);
-  //   }
-  //
-  //   if (prevLeftOthello.match(othelloWhte) && prevOthello.match(othelloBlack)) {
-  //     var targetIndex = index - 1;
-  //     putOthello(targetIndex, index);
-  //   }
-  //
-  //   //右隣の次のマスの色が置いたオセロと同じ色の場合隣のオセロの色を変える
-  //   if (nextRightOthello.match(othelloWhte) && nextOthello.match(othelloBlack)) {
-  //     var targetIndex = index + 1;
-  //     putOthello(targetIndex, index);
-  //   }
-  // }
+  function changeOthello(index) {
+    //両隣とその隣のオセロの色（値）を取得
+    let prevLeftOthello = $tableElements[index - 2].innerHTML;
+    let prevOthello = $tableElements[index - 1].innerHTML;
+    let nextRightOthello = $tableElements[index + 2].innerHTML;
+    let nextOthello = $tableElements[index + 1].innerHTML;
+
+    let unavailableListLeft = [9, 17, 25, 33, 41, 49, 57];
+    let unavailableListRight = [6, 14, 22, 30, 38, 46, 54];
+    const validLeftSide = [];
+    const validRightSide = [];
+    let leftIndex = 0;
+    let rightIndex = 0;
+
+    //配列を使って選択したマスが該当の箇所かをチェックする（左側）
+    unavailableListLeft.forEach(function() {
+      if (index === unavailableListLeft[leftIndex]) {
+        validLeftSide.push(true);
+      } else {
+        validLeftSide.push(false);
+      }
+      leftIndex++;
+    });
+
+    //配列の中に一つでもtrueがあるとtrueを返してくれる「some」メソッド
+    let resultLeftSide = validLeftSide.some((value) => {
+      return value === true;
+    });
+
+    //配列を使って選択したマスが該当の箇所かをチェックする（右側）
+    unavailableListRight.forEach(function() {
+      if (index === unavailableListRight[rightIndex]) {
+        validRightSide.push(true);
+      } else {
+        validRightSide.push(false);
+      }
+      rightIndex++;
+    });
+
+    //配列の中に一つでもtrueがあるとtrueを返してくれる「some」メソッド
+    let resultRightSide = validRightSide.some((value) => {
+      return value === true;
+    });
+
+    //左隣の次のマスの色が置いたオセロと同じ色の場合隣のオセロの色を変える 黒色
+    if (!resultLeftSide) {
+      if (prevLeftOthello.match(othelloBlack) && prevOthello.match(othelloWhte)) {
+        let targetIndex = index - 1;
+        putOthello(targetIndex, index);
+      }
+
+      //左隣の次のマスの色が置いたオセロと同じ色の場合隣のオセロの色を変える 白色
+      if (prevLeftOthello.match(othelloWhte) && prevOthello.match(othelloBlack)) {
+        let targetIndex = index - 1;
+        putOthello(targetIndex, index);
+      }
+    }
+
+    //右隣の次のマスの色が置いたオセロと同じ色の場合隣のオセロの色を変える 黒色
+    if (!resultRightSide) {
+      if (nextRightOthello.match(othelloBlack) && nextOthello.match(othelloWhte)) {
+        let targetIndex = index + 1;
+        putOthello(targetIndex, index);
+      }
+
+      //右隣の次のマスの色が置いたオセロと同じ色の場合隣のオセロの色を変える 白色
+      if (nextRightOthello.match(othelloWhte) && nextOthello.match(othelloBlack)) {
+        let targetIndex = index + 1;
+        putOthello(targetIndex, index);
+      }
+    }
+
+    //相手にオセロを上下で挟み込んだ場合
+  }
 
   //順番の判別する
   function changeOrder() {
